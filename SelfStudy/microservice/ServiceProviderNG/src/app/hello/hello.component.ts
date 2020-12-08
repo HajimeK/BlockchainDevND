525cb3dba31980b3cc61ddc85dea8ab895e38c6d
Kawata <y509541@softwareag.com>
2020-12-08 15:54:46 +0900

    update for backup (FormBuilder, controls...)

--- SelfStudy/microservice/ServiceProviderNG/src/app/hello/hello.component.ts
+++ SelfStudy/microservice/ServiceProviderNG/src/app/hello/hello.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -7,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  title: string;
-  price: number;
-  count: number;
-  message: string;
-  now: Date;
-  input: string;
-  text1: string;
+  title!: string;
+  price!: number;
+  count!: number;
+  message!: string;
+  now!: Date;
+  input!: string;
+  text1!: string;
+  myControl!: FormControl;
+  myGroupControl!: FormGroup;
+  myGroupControl2!: FormGroup;
+  fb!: FormBuilder;
 
   constructor() {
     setInterval(
@@ -29,6 +34,21 @@ export class HelloComponent implements OnInit {
     this.message =  "0 times clicked";
     this.now = new Date();
     this.text1 = '';
+    this.myControl = new FormControl('ok.');
+    this.fb = new FormBuilder();
+    // this.myGroupControl = new FormGroup({
+    //   name: new FormControl(''),
+    //   mail: new FormControl(''),
+    //   age: new FormControl('')
+    // });
+    this.myGroupControl = this.fb.group({
+      name: new FormControl(''),
+      mail: new FormControl(''),
+      age: new FormControl('')
+    });
+    this.myGroupControl2 = new FormGroup({
+      control: new FormControl('')
+    });
   }
 
   today() {
@@ -42,5 +62,18 @@ export class HelloComponent implements OnInit {
   doType(val:string) {
     this.input = val;
     this.message = " You typed:" + this.input;
+  }
+
+  doClick2() {
+    this.message = 'You typed: ' + this.myControl.value
+  }
+
+  onSubmit() {
+    let result = this.myGroupControl.value;
+    this.message = JSON.stringify(result);
+  }
+  onSubmit2() {
+    let result = this.myGroupControl2.value;
+    this.message = JSON.stringify(result);
   }
 }
